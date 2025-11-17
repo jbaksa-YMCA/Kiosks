44e383cc8808f76cd771fac3dbbd90aa2b238a3d
jbaksa-YMCA <jbaksa@ymcadetroit.org>
2025-11-17 12:44:24 -0500

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,44 @@
+chromium --hide-scrollbars --check-for-update-interval=1 --simulate-critical-update --noerrdialogs --disable-infobars --kiosk https://ymcadetroit.org/
+###############################################################
+# * Birmingham *
+#KIOSK
+#chromium --hide-scrollbars --check-for-update-interval=1 --simulate-critical-update --noerrdialogs --disable-infobars --kiosk https://ydev.org/kiosk/displays/birmingham-family-ymca/
+#DONOR
+#chromium --hide-scrollbars --check-for-update-interval=1 --simulate-critical-update --noerrdialogs --disable-infobars --kiosk https://ydev.org/kiosk/displays/birmingham-donors/
+###############################################################
+# * Downriver *
+#KIOSK
+#chromium --hide-scrollbars --check-for-update-interval=1 --simulate-critical-update --noerrdialogs --disable-infobars --kiosk https://ydev.org/kiosk/displays/downriver/
+#DONOR
+#chromium --hide-scrollbars --check-for-update-interval=1 --simulate-critical-update --noerrdialogs --disable-infobars --kiosk https://ydev.org/kiosk/displays/downriver-donors/
+################################################################
+# * Boll *
+#KIOSK
+#chromium --hide-scrollbars --check-for-update-interval=1 --simulate-critical-update --noerrdialogs --disable-infobars --kiosk https://ydev.org/kiosk/displays/boll/
+#DONOR
+#chromium --hide-scrollbars --check-for-update-interval=1 --simulate-critical-update --noerrdialogs --disable-infobars --kiosk https://ydev.org/kiosk/displays/boll-donors/
+################################################################
+# * Carls *
+#KIOSK
+#chromium --hide-scrollbars --check-for-update-interval=1 --simulate-critical-update --noerrdialogs --disable-infobars --kiosk https://ydev.org/kiosk/displays/carls/
+#DONOR
+#chromium --hide-scrollbars --check-for-update-interval=1 --simulate-critical-update --noerrdialogs --disable-infobars --kiosk https://ydev.org/kiosk/displays/carls-donors/
+################################################################
+# * Macomb *
+#KIOSK
+#chromium --hide-scrollbars --check-for-update-interval=1 --simulate-critical-update --noerrdialogs --disable-infobars --kiosk https://ydev.org/kiosk/displays/macomb/
+#DONOR
+#chromium --hide-scrollbars --check-for-update-interval=1 --simulate-critical-update --noerrdialogs --disable-infobars --kiosk https://ydev.org/kiosk/displays/macomb-donors/
+################################################################
+# * South Oakland * 
+#KIOSK
+#chromium --hide-scrollbars --check-for-update-interval=1 --simulate-critical-update --noerrdialogs --disable-infobars --kiosk https://ydev.org/kiosk/displays/south-oakland/
+#DONOR
+#chromium --hide-scrollbars --check-for-update-interval=1 --simulate-critical-update --noerrdialogs --disable-infobars --kiosk https://ydev.org/kiosk/displays/south-oakland-donors/
+################################################################
+# * Farmington *
+#KIOSK
+#chromium --hide-scrollbars --check-for-update-interval=1 --simulate-critical-update --noerrdialogs --disable-infobars --kiosk https://ydev.org/kiosk/displays/farmington-family-ymca/
+#DONOR
+#chromium --hide-scrollbars --check-for-update-interval=1 --simulate-critical-update --noerrdialogs --disable-infobars --kiosk https://ydev.org/kiosk/displays/farmington-donors/
+
